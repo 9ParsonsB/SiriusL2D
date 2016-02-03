488122338d097f40c28e731e1d7e185bf1589e13
Ben Parsons <9parsonsb@gmail.com>
2016-02-03 14:25:35 +0000

    Test Push

--- Game/test.lua
+++ Game/test.lua
@@ -9,17 +9,20 @@ function Create(x, y)
 
     Events.Register("Fire", Fire)
 end
+
 function Update(dt)
     if love.keyboard.isDown("space") then Events.Fire("Fire", dt) end
     if love.keyboard.isDown("w") then Y = Y - Speed * dt end
     if love.keyboard.isDown("s") then Y = Y + Speed * dt end
-    if love.keyboard.isDown("a") then X = X - Speed * dt end	
+    if love.keyboard.isDown("a") then X = X - Speed * dt end
     if love.keyboard.isDown("d") then X = X + Speed * dt end
 end
+
 function Draw()
     Renderer.DrawSprite("greenRect.png", X, Y, Angle)
     Renderer.DrawText("FPS: " .. love.timer.getFPS())
 end
+
 function Fire(dt)
     Timer = Timer + dt
     if Timer > FireRate then
@@ -27,3 +30,4 @@ function Fire(dt)
         Timer = 0
     end
 end
+// Test
