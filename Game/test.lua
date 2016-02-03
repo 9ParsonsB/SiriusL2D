82abbd56ceae3cbf702932dc1d427cbab3336da1
Ben Parsons <9parsonsb@gmail.com>
2016-02-03 14:23:21 +0000

    Minor spacing changes. Test Push

--- Game/test.lua
+++ Game/test.lua
@@ -3,27 +3,27 @@ Timer = 0
 Speed = 300
 
 function Create(x, y)
-	X = x or 0
-	Y = y or 0
-	Angle = 0
+    X = x or 0
+    Y = y or 0
+    Angle = 0
 
-	Events.Register("Fire", Fire)
+    Events.Register("Fire", Fire)
 end
 function Update(dt)
-	if love.keyboard.isDown("space") then Events.Fire("Fire", dt) end
-	if love.keyboard.isDown("w") then Y = Y - Speed * dt end
-	if love.keyboard.isDown("s") then Y = Y + Speed * dt end
-	if love.keyboard.isDown("a") then X = X - Speed * dt end	
-	if love.keyboard.isDown("d") then X = X + Speed * dt end
+    if love.keyboard.isDown("space") then Events.Fire("Fire", dt) end
+    if love.keyboard.isDown("w") then Y = Y - Speed * dt end
+    if love.keyboard.isDown("s") then Y = Y + Speed * dt end
+    if love.keyboard.isDown("a") then X = X - Speed * dt end	
+    if love.keyboard.isDown("d") then X = X + Speed * dt end
 end
 function Draw()
-	Renderer.DrawSprite("greenRect.png", X, Y, Angle)
-	Renderer.DrawText("FPS: " .. love.timer.getFPS())
+    Renderer.DrawSprite("greenRect.png", X, Y, Angle)
+    Renderer.DrawText("FPS: " .. love.timer.getFPS())
 end
 function Fire(dt)
-	Timer = Timer + dt
-	if Timer > FireRate then
-		print("Firing")
-		Timer = 0
-	end
+    Timer = Timer + dt
+    if Timer > FireRate then
+        print("Firing")
+        Timer = 0
+    end
 end
