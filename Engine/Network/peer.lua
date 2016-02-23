c9f73426b6a75163439007b248d57b05cf01b147
Ben Parsons <9parsonsb@gmail.com>
2016-02-23 15:22:45 +0000

    added Peer:SendToServer(data) has P2P handling

--- Engine/Network/peer.lua
+++ Engine/Network/peer.lua
@@ -2,24 +2,41 @@ local socket = require("socket")
 local Peer = Class('Peer')
 
 function Peer:Create(name)
+  self.port = 7253
   self.udp = socket.udp()
   self.udp:settimeout(0)
   self.peername = name -- TODO change to steamID
   self.P2P = false
+  self.server = {}
 end
 
 function Peer:Connect(ip,port)
   self.udp:setpeername(ip,port)
   if self.udp:getsockname() then
+    self.server.ip = ip
+    self.server.port = port
     self.Connected = true
-    return true
+    return  true
+  end
+end
+
+function Peer:SendToServer(data)
+  if self.P2P then
+    if self.Connected then
+      self.udp:sendto(data, self.server.ip, self.server.port)
+    end
+  else
+    if self.udp:send(data) then
+      return true
+    end
+    return
   end
 end
 
 function Peer:Discover()
   if self.udp:setoption('broadcast',true) then
     self.multicast = true
-    local success, msg = udp:sendto("Is anyone there?","255.255.255.255",7253)
+    local success, msg = self.udp:sendto("Is anyone there?","255.255.255.255",self.port)
     if not success then
       --failed
       print("failed to broadcast: "..msg)
