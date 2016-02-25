dfd6a1f8a888c3fbefdd97d2bce3e553116a10ef
Ben Parsons <9parsonsb@gmail.com>
2016-02-25 22:00:13 +0000

    netcode bugfix attempt 2

--- Game/Network/client.lua
+++ Game/Network/client.lua
@@ -3,18 +3,3 @@ function Engine.Client:Start()
   self:Connect('127.0.0.1',7253)
   self.Running = true
 end
-
-function Engine.Client:Update()
-  self.Super.Update(self)
-end
-  
-
-
-function Engine.Client:HandleData(packet) 
-  self.Super.HandleData(self,packet)
-  if self.server.connected then
-    if from == self.server.ip then -- recieved data from authority!
-      print("OUR ALMIGHTY LORD TOLD US SOMETHING")
-    end -- received data from someone else -- TODO: check this with list of client IP's from server 
-  end
-end
