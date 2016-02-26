8e0e66c32923e13240cb63719c25f31e7ac869e4
Ben Parsons <9parsonsb@gmail.com>
2016-02-26 21:37:45 +0000

    fixed bug in creation of netpeer

--- Engine/Network/netpeer.lua
+++ Engine/Network/netpeer.lua
@@ -1,13 +1,12 @@
 local NetPeer = Class("NetPeer")
 
-function NetPeer:Create(ip_or_packet,port,ptype,name,connected)
-  local packet, ip
-  if ip_or_packet.isvaild then packet = ip_or_packet connected = port else ip = ip_or_packet end
+function NetPeer:Create(packet,connected)
+  if not packet.isvaild then return false end
   
-  self.ip = ip or packet
-  self.port = port or packet.port
-  self.peertype = ptype or packet.peertype
-  self.name = name or packet.peername
+  self.ip = packet
+  self.port = packet.port
+  self.peertype = packet.peertype
+  self.name = packet.peername
   self.connected = connected
   self.lastpingtime = packet.receivedtime or 0
   self.ping = packet.receivedtime - packet.senttime or 0
